92dce15d7ec5de882f39aa0452366e838b9ad005
AFTAN18 <aftanfaiz@gmail.com>
2025-05-13 09:33:27 +0530

    Delete 5 directory

--- 5/.py
+++
@@ -1,5 +0,0 @@
-n = int(input("Enter the value of n: "))
-sum_squares = 0
-for i in range(1, n + 1):
-    sum_squares += i * i
-print("Sum of squares of the first", n, "natural numbers is:", sum_square
